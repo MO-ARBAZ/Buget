631c44ba86588a83c9224f2a838b690c29af4ace
Arbaz <arbaz7860095748@gmail.com>
2022-08-22 12:53:20 +0530

    change

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,9 @@
+import React from "react";
+const ExpenseTotal =()=>{
+    return(
+        <div className='alert alert-primary'>
+            <span>Spent so for: $1200</span>
+        </div>
+    )
+}
+export default ExpenseTotal;
